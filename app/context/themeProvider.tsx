673b68b1b49b7cbccd7b7d6f24f984953da91798
achyutem <achyutem.ad@gmail.com>
2025-02-05 19:13:48 +0530

    cleaned for prod

--- app/context/themeProvider.tsx
+++ app/context/themeProvider.tsx
@@ -12,8 +12,8 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
   const getSavedThemeColor = () => {
     try {
       return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
-    } catch (error) {
-      "Zinc" as ThemeColors;
+    } catch {
+      return "Zinc"; // Return "Zinc" if there is an error
     }
   };
 
@@ -46,3 +46,52 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
 export function useThemeContext() {
   return useContext(ThemeContext);
 }
+
+// "use client";
+// import setGlobalColorTheme from "../utils/themeColors";
+// import { useTheme } from "next-themes";
+// import { ThemeProviderProps } from "next-themes";
+// import React, { createContext, useContext, useEffect, useState } from "react";
+
+// const ThemeContext = createContext<ThemeColorStateParams>(
+//   {} as ThemeColorStateParams
+// );
+
+// export default function ThemeDataProvider({ children }: ThemeProviderProps) {
+//   const getSavedThemeColor = () => {
+//     try {
+//       return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
+//     } catch (error) {
+//       "Zinc" as ThemeColors;
+//     }
+//   };
+
+//   const [themeColor, setThemeColor] = useState<ThemeColors>(
+//     getSavedThemeColor() as ThemeColors
+//   );
+//   const [isMounted, setIsMounted] = useState(false);
+//   const { theme } = useTheme();
+
+//   useEffect(() => {
+//     localStorage.setItem("themeColor", themeColor);
+//     setGlobalColorTheme(theme as "light" | "dark", themeColor);
+
+//     if (!isMounted) {
+//       setIsMounted(true);
+//     }
+//   }, [themeColor, theme]);
+
+//   if (!isMounted) {
+//     return null;
+//   }
+
+//   return (
+//     <ThemeContext.Provider value={{ themeColor, setThemeColor }}>
+//       {children}
+//     </ThemeContext.Provider>
+//   );
+// }
+
+// export function useThemeContext() {
+//   return useContext(ThemeContext);
+// }
--- app/utils/themeTypes.ts
+++ app/utils/themeTypes.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 type ThemeColors = "Zinc" | "Rose" | "Blue" | "Green" | "Orange" | "Purple"
 interface ThemeColorStateParams{
     themeColor: ThemeColors;
