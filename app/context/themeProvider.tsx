20cddc4c76c8bba4c5256ca8a7019bbdf0faecab
achyutem <achyutem.ad@gmail.com>
2025-04-23 18:57:42 +0530

    cleanup

--- app/context/themeProvider.tsx
+++ app/context/themeProvider.tsx
@@ -30,6 +30,7 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
     if (!isMounted) {
       setIsMounted(true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [themeColor, theme]);
 
   if (!isMounted) {
@@ -46,52 +47,3 @@ export default function ThemeDataProvider({ children }: ThemeProviderProps) {
 export function useThemeContext() {
   return useContext(ThemeContext);
 }
-
-// "use client";
-// import setGlobalColorTheme from "../utils/themeColors";
-// import { useTheme } from "next-themes";
-// import { ThemeProviderProps } from "next-themes";
-// import React, { createContext, useContext, useEffect, useState } from "react";
-
-// const ThemeContext = createContext<ThemeColorStateParams>(
-//   {} as ThemeColorStateParams
-// );
-
-// export default function ThemeDataProvider({ children }: ThemeProviderProps) {
-//   const getSavedThemeColor = () => {
-//     try {
-//       return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
-//     } catch (error) {
-//       "Zinc" as ThemeColors;
-//     }
-//   };
-
-//   const [themeColor, setThemeColor] = useState<ThemeColors>(
-//     getSavedThemeColor() as ThemeColors
-//   );
-//   const [isMounted, setIsMounted] = useState(false);
-//   const { theme } = useTheme();
-
-//   useEffect(() => {
-//     localStorage.setItem("themeColor", themeColor);
-//     setGlobalColorTheme(theme as "light" | "dark", themeColor);
-
-//     if (!isMounted) {
-//       setIsMounted(true);
-//     }
-//   }, [themeColor, theme]);
-
-//   if (!isMounted) {
-//     return null;
-//   }
-
-//   return (
-//     <ThemeContext.Provider value={{ themeColor, setThemeColor }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// }
-
-// export function useThemeContext() {
-//   return useContext(ThemeContext);
-// }
