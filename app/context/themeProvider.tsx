98a54ba0fcc8f18ca0e83a02a8231e4b698b5b5f
achyutem <achyutem.ad@gmail.com>
2025-07-05 23:35:52 +0530

    defined type for theme color state

--- app/context/themeProvider.tsx
+++ app/context/themeProvider.tsx
@@ -3,47 +3,55 @@ import setGlobalColorTheme from "../utils/themeColors";
 import { useTheme } from "next-themes";
 import { ThemeProviderProps } from "next-themes";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import { ThemeColors } from "../utils/types";
+
+// --- THIS IS THE FIX ---
+// Define the shape of your context's state
+interface ThemeColorStateParams {
+	themeColor: ThemeColors;
+	setThemeColor: React.Dispatch<React.SetStateAction<ThemeColors>>;
+}
 
 const ThemeContext = createContext<ThemeColorStateParams>(
-  {} as ThemeColorStateParams
+	{} as ThemeColorStateParams
 );
 
 export default function ThemeDataProvider({ children }: ThemeProviderProps) {
-  const getSavedThemeColor = () => {
-    try {
-      return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
-    } catch {
-      return "Zinc"; // Return "Zinc" if there is an error
-    }
-  };
+	const getSavedThemeColor = () => {
+		try {
+			return (localStorage.getItem("themeColor") as ThemeColors) || "Zinc";
+		} catch {
+			return "Zinc"; // Return "Zinc" if there is an error
+		}
+	};
 
-  const [themeColor, setThemeColor] = useState<ThemeColors>(
-    getSavedThemeColor() as ThemeColors
-  );
-  const [isMounted, setIsMounted] = useState(false);
-  const { theme } = useTheme();
+	const [themeColor, setThemeColor] = useState<ThemeColors>(
+		getSavedThemeColor() as ThemeColors
+	);
+	const [isMounted, setIsMounted] = useState(false);
+	const { theme } = useTheme();
 
-  useEffect(() => {
-    localStorage.setItem("themeColor", themeColor);
-    setGlobalColorTheme(theme as "light" | "dark", themeColor);
+	useEffect(() => {
+		localStorage.setItem("themeColor", themeColor);
+		setGlobalColorTheme(theme as "light" | "dark", themeColor);
 
-    if (!isMounted) {
-      setIsMounted(true);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [themeColor, theme]);
+		if (!isMounted) {
+			setIsMounted(true);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [themeColor, theme]);
 
-  if (!isMounted) {
-    return null;
-  }
+	if (!isMounted) {
+		return null;
+	}
 
-  return (
-    <ThemeContext.Provider value={{ themeColor, setThemeColor }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+	return (
+		<ThemeContext.Provider value={{ themeColor, setThemeColor }}>
+			{children}
+		</ThemeContext.Provider>
+	);
 }
 
 export function useThemeContext() {
-  return useContext(ThemeContext);
+	return useContext(ThemeContext);
 }
